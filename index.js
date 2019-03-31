fd89e97c2b520e4776962de00a0dd29be9d07b36
James Merkin <jnmerk@gmail.com>
2019-03-31 14:01:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var beatles=[];
 function theBeatlesPlay(musicians,instruments ){
   var counter=0;
   for (counter=0;counter++;counter<(musicians.length)){
- concat (musicians[o]
+ musicians.[0] concat(instruments.[0];
     
   }
   
